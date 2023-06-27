83ddd7d78eb8e335925621e085cf70045189204b
fallvir <137913901+fallvir@users.noreply.github.com>
2023-06-27 19:00:43 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#include <clocale>
+
+#pragma warning (disable: 4996)
+int main()
+{
+	setlocale(0, "");
+	FILE* f1;
+	f1 = fopen("in1.txt", "r");
+	int n;
+	fscanf_s(f1, "%d", &n);
+	int* mas1 = new int[n];
+	for (int i = 0; i < n; i++)
+	{ 
+		fscanf_s(f1, "%d ", &mas1[i]);
+	}
+
+	fclose(f1);
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", mas1[i]);
+	}
+
+
+
+
+	return 0;
+}
+
